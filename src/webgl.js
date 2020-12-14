4ff0e16e7a3354fe173c48edcb3ad121bbe70111
Gregg King <gregg.king@redspace.com>
2020-12-14 01:47:54 -0400

    Remove redundant GL call.

--- src/webgl.js
+++ src/webgl.js
@@ -1011,7 +1011,6 @@ export function main(player, canvas, options) {
 
   // set default clearing values
   gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-  gl.clearDepth(1.0); // Clear everything
   gl.enable(gl.DEPTH_TEST); // Enable depth testing
   gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
