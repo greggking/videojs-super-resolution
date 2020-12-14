8b1b0f5eaff4a2a3c3e00d51484c64cf159fb801
Gregg King <gregg.king@redspace.com>
2020-12-14 06:49:44 -0400

    Remove the if-else in the padFragShader and replace with a step function. Reduce the number of variables allocated.

--- src/webgl.js
+++ src/webgl.js
@@ -201,21 +201,17 @@ function initPadProgram(gl, padding) {
   uniform sampler2D originalSampler;
   uniform vec2 videoRes;
 
-  ivec2 start = ivec2(${padding}, ${padding});
+  vec2 padding = vec2(${padding});
   layout(location = 0) out vec4 padOut;
 
   void main() {
-    ivec2 end = ivec2(videoRes.x + ${padding}.0, videoRes.y + ${padding}.0);
-    ivec2 outC = ivec2(gl_FragCoord.x, gl_FragCoord.y);
+    // mask.xy == (1.0, 1.0) when inside the padding
+    vec2 mask = step(padding.xy, gl_FragCoord.xy) - step(videoRes.xy + padding, gl_FragCoord.xy);
 
-    if (any(lessThan(outC, start)) || any(greaterThanEqual(outC, end))) {
-      padOut = vec4(0.0, 0.0, 0.0, 0.0);
-    } else {
-      float x = gl_FragCoord.x - 4.0;
-      float y = gl_FragCoord.y - 4.0;
-      vec2 coords = vec2(x / videoRes.x, y / videoRes.y);
-      padOut = texture(originalSampler, coords) * 255.0;
-    }
+    // adjust texture coords to account for padding
+    vec2 coords = (padding * vec2(-1.0) + gl_FragCoord.xy) / videoRes.xy;    
+
+    padOut = mask.x * mask.y * texture(originalSampler, coords) * 255.0;
   }
   `;
 
